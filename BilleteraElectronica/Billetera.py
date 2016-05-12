b6f552ab1c96fd86afcf5e42637dd69be38d8370
carlos <carlos@asus.ldc.usb.ve>
2016-05-12 12:00:23 -0430

    Pruebas

--- BilleteraElectronica/Billetera.py
+++ BilleteraElectronica/Billetera.py
@@ -3,7 +3,15 @@ Created on May 12, 2016
 
 @author: carlos
 '''
+class dueno:
+    def __init__(self, nombres, apellidos, ci):
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.ci = ci
+
 class billeteraElectronica:
-    def __init__(self, identificador):
+    def __init__(self, identificador, dueno, pin):
         self.identificador = identificador
+        self.dueno = dueno
+        self.pin = pin
     
--- BilleteraElectronica/Pruebas.py
+++ BilleteraElectronica/Pruebas.py
@@ -7,7 +7,16 @@ from Billetera import *
 import unittest
 
 class PruebasBilletera(unittest.TestCase):
+    '''
     def testClaseVacia(self):
         bill = billeteraElectronica('255')
-        
+    '''   
+    ''' 
+    def testClaseDueno(self):
+        Angel = dueno('Angel', 'Martinez Gonzalez', 21134902)
+        bill = billeteraElectronica('255', Angel)
+    '''    
+    def testPin(self):
+        Angel = dueno('Angel', 'Martinez Gonzalez', 21134902)
+        bill = billeteraElectronica('255', Angel, 2201)
         
